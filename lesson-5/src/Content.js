9249fe88fef46cd85cfcb46d343b4094683ccc4a
Nikola <9nikolov@gmail.com>
2024-03-13 01:16:32 +0200

    adding finished lesson 5 folder

--- lesson-5/src/Content.js
+++ lesson-5/src/Content.js
@@ -9,10 +9,28 @@ const Content = () => {
         return names[pickRandomName];
       };
 
+  const handleClick = () => {
+    console.log('You clicked it');
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+
+  const handleDoubleClick = () => {
+    console.log("BUTTON 3 got double clicked!");
+  }
 
   return (
     <main>
         <p>Hello {handleNameChange()}</p>
+        <button onClick={handleClick}>Click it</button>
+        <br/>
+        <button onClick={() => handleClick2('Dave')}>Click it</button>
+        <br/>
+        {/* TESTING GUIDE: Double Click does not work when Google Chrome Inspector Tools are open
+            SOLUTION: double click the button, then open inspect menu to see the result in the console */}
+        <button onDoubleClick={handleDoubleClick}>BUTTON 3</button>
     </main>
   )
 }
